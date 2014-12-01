34b3196a54fb4412d6fe66d080ae51a2e7b17efa
Essam Al Joubori <essam.joubori@gmail.com>
2014-12-01 18:49:13 -0500

    implement NPV function

--- public/features/controllers/npvController.js
+++ public/features/controllers/npvController.js
@@ -1,3 +1,35 @@
-function NpvCtrl (){
-  console.log("heeloooo");
-}
+function NpvCtrl ($scope, $http){
+
+  var npvFunction = function (rate, cf0) {
+    console.log('called', arguments)
+    var rate = rate/100, npv = cf0;
+    for (var i = 2; i < arguments.length; i++){
+      npv +=(arguments[i] / Math.pow((1 + rate), i - 1));
+    }
+    console.log(npv*100)
+    return (Math.round(npv * 100) / 100);
+  };
+
+
+  $scope.calculateNPV = function(){
+    console.log($scope.npv);
+    var rate = $scope.npv.rate;
+    var cf0 = $scope.npv.cf0;
+    var cf1 = $scope.npv.cf1;
+    var cf2 = $scope.npv.cf2;
+    var cf3 = $scope.npv.cf3;
+
+    var npvValue = npvFunction(Number(rate), Number(cf0), Number(cf1), Number(cf2), Number(cf3));
+    console.log('npvValue', npvValue);
+    
+    
+
+  };
+
+  $scope.renderNPV = function (response){
+    $scope.npv = response; 
+  };
+
+  $http.get('/npvs')
+    .success($scope.renderNPV);
+};
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
 
+var db = mongojs("npvdb", ["npvs"])
+var app = express();
+
+
+// app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   render('index')
-})
+});
+
+app.get('/npvs', function (req, res) {
+  res.send([]);
+});
+
+app.post('/npvs', function (req, res) {
+  // var npv = req.body;
+  // console.log(npv);
+  db.npvs.insert(req.body, function(err, data){
+    console.log(data);
+    res.end(JSON.stringify(data));
+  }); 
+});
+
+
 
 var server = app.listen(3000, function () {
 
